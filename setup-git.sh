4c199fd574baa7540b6e8443c1f35d9bc61db102
Eric Yu <eric.yu@iclink.tw>
2018-02-12 09:46:08 +0000

    new file:   install.sh new file:   setup-git.sh

---
+++ setup-git.sh
@@ -0,0 +1,11 @@
+git config --global core.editor vim
+
+git config --global merge.tool vimdiff
+git config --global mergetool.prompt false
+git config --global alias.mt mergetool
+
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+git config --global alias.d difftool
+
+git config --global push.default simple
