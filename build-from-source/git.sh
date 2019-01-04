49f4eb99d09555ed191695a651167bffa17f481e
Eric Yu <ffk0716@gmail.com>
2019-01-04 16:42:00 +0000

    modified:   ../build-from-source/git.sh modified:   ../shell/mainrc

--- build-from-source/git.sh
+++ build-from-source/git.sh
@@ -4,7 +4,7 @@ set -eux
 sudo apt-get install dh-autoreconf libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev
 sudo apt-get install asciidoc xmlto docbook2x
 sudo apt-get install install-info
-sudo apt-get install zlib libz-dev gettext
+sudo apt-get install libz-dev gettext
 
 wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.20.1.tar.gz
 tar -xf git-2.20.1.tar.gz
