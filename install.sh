74b4f053ad5645f03407cab46c016637ce7b0dce
Eric Yu <ffk0716@gmail.com>
2021-03-13 02:53:38 +0800

    Use Vundle

--- install.sh
+++ install.sh
@@ -27,8 +27,7 @@ popd > /dev/null
 
 echo
 echo setup vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 
