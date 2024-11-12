7ffc34848ea47a44faf90082365138f15d17aa29
Eric Yu <ffk0716@gmail.com>
2024-11-13 01:19:49 +0800

    modified:   install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,3 +1,6 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/bhilburn/powerlevel9k.git     ~/.oh-my-zsh/custom/themes/powerlevel9k
 brew install --cask font-sauce-code-pro-nerd-font
+
+# for "iTerm2-Color-Schemes/schemes/Monokai Soda.itermcolors"
+git clone git@github.com:mbadolato/iTerm2-Color-Schemes.git
