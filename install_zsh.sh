5a886d23fc852edef7469a415f454a3509021f63
Eric Yu <ffk0716@gmail.com>
2024-11-13 01:09:02 +0800

    modified:   install_zsh.sh

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/bhilburn/powerlevel9k.git     ~/.oh-my-zsh/custom/themes/powerlevel9k
+brew install --cask font-sauce-code-pro-nerd-font
