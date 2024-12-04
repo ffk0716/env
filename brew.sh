cf6c981ce7227f823989b6408ece6205e7344725
Eric Yu <ffk0716@gmail.com>
2024-12-04 23:42:35 +0800

    Add stats
    modified:   brew.sh

--- brew.sh
+++ brew.sh
@@ -2,7 +2,8 @@
 set -eux
 brew install --cask itsycal
 brew install --cask jordanbaird-ice
-brew install expect
-brew install python
+brew install --cask stats
 brew install ctags
 brew install exiftool
+brew install expect
+brew install python
